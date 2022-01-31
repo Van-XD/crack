b6aa31712215bd50c7d4312da12020ebd0ddf7bc
Ivan XD <49156341+Van-XD@users.noreply.github.com>
2022-01-31 17:28:03 +0700

    Update crack.py

--- crack.py
+++ crack.py
@@ -84,11 +84,11 @@ def tod():
         time.sleep(1)
 
 # LO KONTOL
-logo = '''\x1b[1;97m    ___ __  __ ____  _____
-\x1b[1;97m    |_ _|  \/  | __ )|  ___| |\x1b[1;96m® \x1b[1;97mAuthor  \x1b[1;91m: \x1b[1;90mIvanTek
-\x1b[1;97m     | || |\/| |  _ \| |_ |\x1b[1;96m® \x1b[1;97mYoutube \x1b[1;91m: \x1b[1;92mIvanYT
-\x1b[1;97m     | || |  | | |_) |  _| |\x1b[1;96m® \x1b[1;97mGithub  \x1b[1;91m: \x1b[1;97m/Van-XD
-\x1b[1;97m    |___|_|  |_|____/|_| |\x1b[1;96m® \x1b[1;97mVersion \x1b[1;91m: \x1b[1;90m1.0
+logo = '''\x1b[1;97m   ___ __  __ ____  _____
+\x1b[1;97m   |_ _|  \/  | __ )|  ___|  |\x1b[1;96m® \x1b[1;97mAuthor  \x1b[1;91m: \x1b[1;90mIvanTek
+\x1b[1;97m    | || |\/| |  _ \| |_     |\x1b[1;96m® \x1b[1;97mYoutube \x1b[1;91m: \x1b[1;92mIvanYT
+\x1b[1;97m    | || |  | | |_) |  _|    |\x1b[1;96m® \x1b[1;97mGithub  \x1b[1;91m: \x1b[1;97m/Van-XD
+\x1b[1;97m   |___|_|  |_|____/|_|      |\x1b[1;96m® \x1b[1;97mVersion \x1b[1;91m: \x1b[1;90m1.0
                        '''
 
 lo_ngentod = '2279647272211275'
